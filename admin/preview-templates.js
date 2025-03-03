016c2dbfcae62d78a54348dc8189db6d52bb811f
alpha19970221 <alpha19970221@users.noreply.github.com>
2025-03-03 12:14:53 +0000

    deploy: a40c13a42ab66c684b817b11cc1a59efa0c199b1

---
+++ admin/preview-templates.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CMS from 'netlify-cms-app'
+
+// 定义你的预览组件，比如用于文章集合的预览
+const PostPreview = ({ entry, widgetFor }) => {
+  const data = entry.toJS()
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <div>{widgetFor('body')}</div>
+    </div>
+  )
+}
+
+// 注册预览模板
+CMS.registerPreviewTemplate('posts', PostPreview)
