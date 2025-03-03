ff1bf9034afbb99e43e9412840103b760672f318
Alpha19970221 <makingzzy@163.com>
2025-03-03 21:44:43 +0800

    fix:修复预览样式配置

--- static/admin/preview-templates.js
+++ static/admin/preview-templates.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CMS from 'decap-cms-app'
+// 注入外部 CSS 高亮样式（例如 Prism Tomorrow 主题）
+DecapCMS.registerPreviewStyle('https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css');
 
-// 定义你的预览组件，比如用于文章集合的预览
-const PostPreview = ({ entry, widgetFor }) => {
-  const data = entry.toJS()
-  return (
-    <div>
-      <h1>{data.title}</h1>
-      <div>{widgetFor('body')}</div>
-    </div>
-  )
+// 使用原生 JavaScript 创建预览组件
+function PostPreview(props) {
+  // 获取文章数据
+  var data = props.entry.toJS();
+  return React.createElement(
+    "div",
+    { style: { padding: "1em", fontFamily: "Arial, sans-serif" } },
+    React.createElement("h1", null, data.title),
+    React.createElement("p", null, data.date),
+    React.createElement("div", null, props.widgetFor("body"))
+  );
 }
 
-// 注册预览模板
-CMS.registerPreviewTemplate('posts', PostPreview)
+// 注册预览模板，'posts' 应与 config.yml 中定义的集合名称一致
+DecapCMS.registerPreviewTemplate("posts", PostPreview);
